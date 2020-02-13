ba6571b3fedfe51c52ed73405f949a2f07e7a077
Insidium <ryanchallen@outlook.com>
2020-02-13 11:56:27 +1000

    Added average overall rating logic

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,6 +7,7 @@ class BooksController < ApplicationController
         @book = Book.find(params[:id])
         @can_add = !BookListItem.contains?(current_user, @book) if user_signed_in?
         @reviews = @book.reviews
+        @rating = Review.get_average_rating(@book)
     end
 
     def add
